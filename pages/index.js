0721bf0df8df4c2cfe6fe3660a8aea20545dab9d
ivanfabriano <ivanfabriano99@mail.ugm.ac.id>
2022-09-07 07:58:30 +0700

    Add showcase image galery

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState, useEffect } from "react";
 import Typewriter from "typewriter-effect";
 import Image from "next/image";
 
@@ -7,15 +9,36 @@ import {
   DataUsageOutlined, 
   PaymentsOutlined,
   MoreOutlined,
-  Circle 
+  Circle,
+  PlayCircleFilledWhiteOutlined 
 } from '@mui/icons-material';
 
 import service1 from '../public/assets/images/services1.svg'
 import service2 from '../public/assets/images/services2.svg'
 import service3 from '../public/assets/images/services3.svg'
 import porto1_1 from '../public/assets/images/porto1_1.png'
+import porto1_2 from '../public/assets/images/porto1_2.png'
+import porto1_3 from '../public/assets/images/porto1_3.png'
+import porto1_4 from '../public/assets/images/porto1_4.png'
 
 const index = () => {
+  const [showImage, setShowImage] = useState(porto1_1);
+
+  const showUpImage = (e) => {
+    const name = e.target.name;
+
+    if(name === 'porto1_1') setShowImage(porto1_1);
+    if(name === 'porto1_2') setShowImage(porto1_2);
+    if(name === 'porto1_3') setShowImage(porto1_3);
+    if(name === 'porto1_4') setShowImage(porto1_4);
+  }
+
+  // useEffect(() => {
+  //   setTimeout(()=>{
+  //     setShowImage(porto1_1);
+  //   },5000)
+  // }, [showImage])
+
   return (
     <div className="py-6 overflow-hidden">
       <div id="hero-page" className="py-12 px-10">
@@ -89,19 +112,49 @@ const index = () => {
         <div className="w-0 h-0 border-l-[1000px] border-l-transparent border-b-[250px] border-b-primary-text border-r-[1000px] border-r-transparent"></div>
       </div>
       <div className="bg-primary-text text-primary-white text-center pb-24">
-          <h2 className="text-3xl font-semibold mb-20">Our Product in 2022</h2>
-          <div className="flex my-6 px-6">
-            <div className="w-1/2 px-4">
-              <div className="">
+          <h2 className="text-3xl font-semibold mb-20">Our Products in 2022</h2>
+          <div className="my-6 px-6 flex ">
+            <div className="w-1/2 px-4 inline-block">
+              <div className="pl-10">
                 <Image
-                    src={porto1_1}
+                    src={showImage}
                     alt="Picture of the author"
+                    className="rounded-xl"
                 />
+                {/* <div className="absolute left-10 right-0 bottom-1 text-primary-text py-2">
+                  <button className="rounded-lg mx-2 px-2 py-1 border-2 border-primary-text bg-primary-white"><PlayCircleFilledWhiteOutlined /> Try Demo</button>
+                </div> */}
+                <div className="flex">
+                  <div className="w-1/3 py-2 px-1 cursor-pointer" onClick={(e) => showUpImage(e)}>
+                    <Image
+                      src={porto1_2}
+                      alt="Picture of the author"
+                      className="rounded-lg"
+                      name="porto1_2"
+                    />
+                  </div>
+                  <div className="w-1/3 py-2 px-1 cursor-pointer" onClick={(e) => showUpImage(e)}>
+                    <Image
+                      src={porto1_3}
+                      alt="Picture of the author"
+                      className="rounded-lg"
+                      name="porto1_3"
+                    />
+                  </div>
+                  <div className="w-1/3 py-2 px-1 cursor-pointer" onClick={(e) => showUpImage(e)}>
+                    <Image
+                      src={porto1_4}
+                      alt="Picture of the author"
+                      className="rounded-lg"
+                      name="porto1_4"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="w-1/2 px-4">
-              <div className="text-left">
-                <h2 className="text-xl font-semibold mb-4">Human Resource Information System</h2>
+            <div className="w-1/2 px-4 flex">
+              <div className="w-8/12 text-left pl-2">
+                <h2 className="text-xl font-semibold mb-4 pl-2">Human Resource Information System</h2>
                 <div className="border border-primary-white rounded-xl w-max">
                   <h3 className="py-2 px-3 bg-primary-white rounded-t-xl text-primary-text font-semibold">What can you get ? <Circle className="float-right text-sm"/><Circle className="float-right text-sm"/><Circle className="float-right text-sm"/></h3>
                   <ul className="px-3">
@@ -110,6 +163,12 @@ const index = () => {
                     <li className="my-3"><PaymentsOutlined className="mr-3"/>Detailed employee payroll records</li>
                     <li className="my-3"><MoreOutlined className="mr-3"/>And more best features ... </li>
                   </ul>
+                </div>
+              </div>
+              <div className="w-4/12 text-primary-white pt-28">
+                <div>
+                  <PlayCircleFilledWhiteOutlined className="text-[80px]"/>
+                  <h4>Play Demo</h4>
                 </div>
               </div>
             </div>
